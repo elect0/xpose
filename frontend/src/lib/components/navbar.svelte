f372f8a786860ed9505998eddb2fd1dd3113b825
elect0 <educinca8@gmail.com>
2025-12-23 17:50:11 +0200

    fix: fix hero section for landing page

--- frontend/src/lib/components/navbar.svelte
+++ frontend/src/lib/components/navbar.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <nav
-	class="fixed top-0 right-0 left-0 z-50 w-full border-b-2 border-black bg-[#F4E6CF] py-1 md:py-3"
+	class="fixed top-0 right-0 left-0 z-50 w-full bg-transparent py-1 md:py-3"
 >
 	<div class="container mx-auto px-4 sm:px-6 lg:px-8">
 		<div class="flex h-16 items-center justify-between">
@@ -16,20 +16,20 @@
 					<div class="text-[#117CD9]] text-2xl font-bold md:text-3xl">LI.</div>
 				</a>
 			</div>
-			<div class="space-x-3">
-				<a
-					href="/the-loop"
-					class={page.url.pathname.startsWith('/the-loop') ? 'text-[#F25C69]' : 'text-[#0C0202]'}
-					>The Loop</a
-				>
-				<a
-					href="/packs"
-					class={page.url.pathname.startsWith('/packs') ? 'text-[#F25C69]' : 'text-[#0C0202]'}
-					>Packs</a
-				>
-			</div>
+			<!-- <div class="space-x-3"> -->
+			<!-- 	<a -->
+			<!-- 		href="/the-loop" -->
+			<!-- 		class={page.url.pathname.startsWith('/the-loop') ? 'text-[#F25C69]' : 'text-[#0C0202]'} -->
+			<!-- 		>The Loop</a -->
+			<!-- 	> -->
+			<!-- 	<a -->
+			<!-- 		href="/packs" -->
+			<!-- 		class={page.url.pathname.startsWith('/packs') ? 'text-[#F25C69]' : 'text-[#0C0202]'} -->
+			<!-- 		>Packs</a -->
+			<!-- 	> -->
+			<!-- </div> -->
 			<button
-				class="transform cursor-pointer rounded-md border-2 border-black bg-[#117CD9] px-4 py-2 text-sm font-semibold text-black shadow-[2px_2px_0_0_#000] transition-all hover:translate-x-1 hover:translate-y-1 hover:shadow-none active:translate-x-1 active:translate-y-1 active:shadow-none md:text-base"
+				class="transform cursor-pointer rounded-full border-2 border-black bg-[#FFFBEF] px-4 py-2 text-sm font-semibold text-black shadow-[2px_2px_0_0_#000] transition-all hover:translate-x-1 hover:translate-y-1 hover:shadow-none active:translate-x-1 active:translate-y-1 active:shadow-none md:text-base"
 			>
 				Ready up!
 			</button>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,47 +1,29 @@
 <script>
-	import { browser } from '$app/environment';
-	import Scene from '$lib/components/scene.svelte';
+	import EduImage from '$lib/assets/edu.png';
 </script>
 
-<div class="relative flex min-h-screen items-center justify-center overflow-hidden bg-[#FAF5EC]">
-	<div class="relative z-10 container mx-auto px-4 pt-28 lg:px-6">
-		<h1 class="mb-6 text-center text-8xl font-bold text-[#1468C1]">Time to call 'em out.'</h1>
-		<div class="svg-grid grid grid-cols-2 gap-8 p-12">
-			<div class="block space-y-4">
-				<div
-					class="rounded-md border-3 border-black bg-[#FAF5EC] px-5 py-3 text-sm font-semibold text-black shadow-[4px_4px_0_0_#F25C69] md:text-lg"
-				>
-					[Player 1]
-				</div>
+<div class="grid-bg flex max-h-screen justify-center bg-[#CFFB80] pt-40 text-center">
+	<div class="flex flex-col items-center space-y-5 px-4">
+		<div
+			class="max-w-2xl rounded-full border-2 border-black bg-[#FFFBEF] px-4 py-2 text-sm font-semibold text-black shadow-[2px_2px_0_0_#000]"
+		>
+The Ultimate Friendship Ruining App
+		</div>
 
-				<div
-					class="rounded-md border-3 border-black bg-[#FAF5EC] px-5 py-3 text-sm font-semibold text-black shadow-[4px_4px_0_0_#F25C69] md:text-lg"
-				>
-					[Player 1]
-				</div>
-
-				<div
-					class="rounded-md border-3 border-black bg-[#FAF5EC] px-5 py-3 text-sm font-semibold text-black shadow-[4px_4px_0_0_#F25C69] md:text-lg"
-				>
-					[Player 1]
-				</div>
-
-				<button
-					class="w-full transform cursor-pointer rounded-md border-2 border-black bg-[#F25C69] px-5 py-3 text-lg font-semibold text-black shadow-[4px_4px_0_0_#000] transition-all hover:translate-x-1 hover:translate-y-1 hover:shadow-none active:translate-x-1 active:translate-y-1 active:shadow-none"
-				>
-					Let's Play!
-				</button>
-			</div>
-			<img src="/controller.png" alt="" />
+		<h1 class="text-7xl font-bold">
+			Vote, Roast, Expose, Laugh. All in <br> One App.
+		</h1>
+		<p class="max-w-4xl text-lg">
+			Forget safe conversations. Every 24 hours, a new question drops. Accuse your friends, lock in
+			your votes, and check the receipts to see who the group really thinks is the problem.
+		</p>
+		<button
+			class="transform cursor-pointer rounded-full border-2 border-black bg-[#FFFBEF] px-6 py-3 text-lg font-semibold text-black shadow-[2px_2px_0_0_#000] transition-all hover:translate-x-1 hover:translate-y-1 hover:shadow-none active:translate-x-1 active:translate-y-1 active:shadow-none"
+		>
+			Enter the lobby.
+		</button>
+		<div class="mt-4 ml-8 h-[px] overflow-hidden">
+			<enhanced:img src={EduImage ? EduImage : ''} alt="Phone image" class="w-64 md:w-80 lg:w-96" />
 		</div>
 	</div>
 </div>
-
-<style>
-	.hero-3d {
-		width: 100%;
-		height: 300px; /* Ensure the parent has height */
-		border-radius: 1rem;
-		overflow: hidden;
-	}
-</style>
